f3113a4d339a8b73e7d74d537b759beee754891c
nawabsahab16 <Nawab162004@gmail.com>
2024-06-09 09:30:29 +0530

    feat: setting up user's currency 💸

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import { ThemeProvider } from 'next-themes';
- import React, { ReactNode } from 'react'
+import React, { ReactNode } from 'react'
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
  
- function RootProviders({children} : {children : ReactNode}) {
+ function RootProviders({children} : {children : ReactNode}) { 
+
+   const [queryClient] = React.useState(() => new QueryClient({}));
    return (
+      <QueryClientProvider client={queryClient}>
      <ThemeProvider 
         attribute = "class"
         defaultTheme='dark'
         enableSystem 
         disableTransitionOnChange
      >
-         {children}
-     </ThemeProvider>
+       {children}
+     </ThemeProvider> 
+      <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
    )
  }
  
