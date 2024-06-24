8e86a941a1c282ebbacfbf290680bea161c0d459
nawabsahab16 <Nawab162004@gmail.com>
2024-06-24 16:53:17 +0530

    feat: implemented dateRange for user to get the details, within the range window

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,6 +4,7 @@ import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation';
 import React from 'react'
 import CreateTransactionDialog from './_components/CreateTransactionDialog';
+import Overview from './_components/Overview';
 
 async function page(){ 
 
@@ -50,9 +51,9 @@ async function page(){
          type = "expense"
         />      
          </div>
-
-        </div>
-     </div>
+        </div> 
+     </div> 
+       <Overview userSettings={userSettings} />
     </div>
   )
 }
